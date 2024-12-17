64c4adc8d5c08b8e0d18a18c90e69b1c83b21bff
Luciano Equiel Wagner <luciano2004wagner@gmail.com>
2024-12-17 18:37:37 -0300

    termino el navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 
-import LoginComponent from './components/login/login.component';
-import {HomeComponent} from './components/home/home.component';
-import { AuthGuard } from './core/auth/auth.guard';
+import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { 
-        path: 'home', 
-        component: HomeComponent,
-        canActivate: [AuthGuard], // Protege la ruta home
-    },
-    { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a login por defecto
-    { path: '**', redirectTo: '/login' }, // Redirige cualquier ruta desconocida a login
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
+  { path: '**', redirectTo: 'home' }, // Redirige cualquier ruta desconocida a home
 ];
 
-export const routerProviders = provideRouter(routes);   
+export const routerProviders = provideRouter(routes);
