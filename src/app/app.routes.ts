d860506cca9826ff9584866e2f1b70301c684b6d
Luciano Equiel Wagner <luciano2004wagner@gmail.com>
2024-12-18 10:20:32 -0300

    soluciono todo el problema de los navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,22 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import  LoginComponent  from './components/login/login.component';
 import { AppComponent } from './app.component';
+import {LayoutComponent} from './components/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: LayoutComponent ,
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
+
     ],
   },
-  { path: '**', redirectTo: 'home' }, // Redirige cualquier ruta desconocida a home
+  {path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: '' }, // Redirige cualquier ruta a 404
 ];
 
 export const routerProviders = provideRouter(routes);
